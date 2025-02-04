ca1d8aecc23267b98a1fda9eb428cbec0ccd1a00
Emindgn0 <m.emind3458@gmail.com>
2025-02-04 20:16:39 +0300

    matrislerin toplamı eklendi

--- Matris_Transpoz/src/Main.java
+++ Matris_Transpoz/src/Main.java
@@ -3,34 +3,52 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Matrisin Satır Sayısını Giriniz: ");
-        int satir = scanner.nextInt();
-        System.out.print("Matrisin Sütun Sayısını Giriniz: ");
-        int sutun = scanner.nextInt();
 
-        int[][] matrix = new int[satir][sutun];
+        // İlk matrisi alalım
+        System.out.print("Birinci matrisin Satır Sayısını Giriniz: ");
+        int satir1 = scanner.nextInt();
+        System.out.print("Birinci matrisin Sütun Sayısını Giriniz: ");
+        int sutun1 = scanner.nextInt();
 
-        System.out.println("Matrisin Elemanlarını Giriniz:");
-        for (int i = 0; i < satir; i++) {
-            for (int j = 0; j < sutun; j++) {
-                matrix[i][j] = scanner.nextInt();
+        int[][] matrix1 = new int[satir1][sutun1];
+
+        System.out.println("Birinci Matrisin Elemanlarını Giriniz:");
+        for (int i = 0; i < satir1; i++) {
+            for (int j = 0; j < sutun1; j++) {
+                matrix1[i][j] = scanner.nextInt();
             }
         }
-        System.out.println("Orijinal Matris:");
-        printMatrix(matrix);
+        System.out.println("Birinci Matris:");
+        printMatrix(matrix1);
 
 
-        int[][] transpose = new int[sutun][satir];
+        System.out.print("İkinci matrisin Satır Sayısını Giriniz: ");
+        int satir2 = scanner.nextInt();
+        System.out.print("İkinci matrisin Sütun Sayısını Giriniz: ");
+        int sutun2 = scanner.nextInt();
 
-        for (int i = 0; i < satir; i++) {
-            for (int j = 0; j < sutun; j++) {
-                transpose[j][i] = matrix[i][j];
+        int[][] matrix2 = new int[satir2][sutun2];
+
+        System.out.println("İkinci Matrisin Elemanlarını Giriniz:");
+        for (int i = 0; i < satir2; i++) {
+            for (int j = 0; j < sutun2; j++) {
+                matrix2[i][j] = scanner.nextInt();
             }
         }
-        System.out.println("Transpoze Matris:");
-        printMatrix(transpose);
+        System.out.println("İkinci Matris:");
+        printMatrix(matrix2);
+
+        // Matris toplamı
+        if (satir1 == satir2 && sutun1 == sutun2) {
+            int[][] sum = addMatrices(matrix1, matrix2);
+            System.out.println("Matris Toplamı:");
+            printMatrix(sum);
+        } else {
+            System.out.println("Matris boyutları uyuşmuyor, toplama yapılamaz.");
+        }
     }
 
+    // Matrisleri ekrana yazdıran fonksiyon
     public static void printMatrix(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
@@ -38,5 +56,19 @@ public class Main {
             }
             System.out.println();
         }
+    }
+
+    // İki matrisin toplamını hesaplayan fonksiyon
+    public static int[][] addMatrices(int[][] matrix1, int[][] matrix2) {
+        int satir = matrix1.length;
+        int sutun = matrix1[0].length;
+        int[][] sum = new int[satir][sutun];
+
+        for (int i = 0; i < satir; i++) {
+            for (int j = 0; j < sutun; j++) {
+                sum[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        return sum;
     }
 }
