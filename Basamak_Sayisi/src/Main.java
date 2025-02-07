d97f823888f7f26f81fcdd40511b77a2c800f329
Emindgn0 <m.emind3458@gmail.com>
2025-02-07 18:39:51 +0300

    basamak sayısı toplamı eklendi
    Signed-off-by: Muhammed Emin <anafartalar12@gmail.com>

--- Basamak_Sayisi/src/Main.java
+++ Basamak_Sayisi/src/Main.java
@@ -5,16 +5,25 @@ public class Main {
         Scanner scanner=new Scanner(System.in);
         System.out.print("Bir Sayı Giriniz : ");
         int number = scanner.nextInt(); // Kullanıcıdan alınan sayı
+        number = Math.abs(number);//negatif sayıları pozitife çevirir (mutlak değer )
         int digitCount=0; // Sayac
+        int digitSum=0; // Basamak toplamı
+        int tempNumber=number;
+
+        System.out.println("Basamaklar : " );
         do {
-            number=number/10; //Sayı her bölündüğünde bir basamak azalır
-            digitCount++; //10'a her bölündüğünde sayaç 1 artar 
-        }while(number!=0);
+            int digit = tempNumber %10; // son basamağı al
+            System.out.println(digit + " ");
+            digitSum+=digit;
+            tempNumber=tempNumber /10; //Sayı her bölündüğünde bir basamak azalır
+            digitCount++; //10'a her bölündüğünde sayaç 1 artar
+        }while(tempNumber!=0);
         System.out.println("Basamak Sayısı : "+digitCount);
+        System.out.println("Basamak Toplamı : "+digitSum);
 
         //Program girilen sayının 10'a kaç defa bölünebileceğini sayıyor böylece basamak sayısını buluyoruz
 
 
-        }
-
     }
+
+}
